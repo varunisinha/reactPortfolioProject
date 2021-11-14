925a0fc90f44d73a4d222f45078f3677f011f1be
varunisinha <varuni86@gmail.com>
2021-11-14 15:54:36 -0500

    build

---
+++ build/precache-manifest.d4826ed844de568ac3715bc36db29b4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62691fb5313f368aea91922a808baf2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2278ebe78861a2e9bdab",
+    "url": "/static/css/main.88ec6981.chunk.css"
+  },
+  {
+    "revision": "31ee569f9352addec496",
+    "url": "/static/js/2.6af57899.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6af57899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2278ebe78861a2e9bdab",
+    "url": "/static/js/main.425cba66.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "ffe93b1e9bd0587367d69b102794bcce",
+    "url": "/static/media/profile.ffe93b1e.jpg"
+  }
+]);
